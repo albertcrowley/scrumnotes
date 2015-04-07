5282e217c19cda7c9461435a43108f0f92297ba0
Al <al@bwnj.net>
2015-04-07 12:31:41 -0400

    Pulling mail now

--- ScrumNotes/Form1.Designer.cs
+++ ScrumNotes/Form1.Designer.cs
@@ -28,30 +28,32 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.bGo = new System.Windows.Forms.Button();
+            this.tbEmail = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.tbPassword = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.rtbOutput = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbServer = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // button1
+            // bGo
             // 
-            this.button1.Location = new System.Drawing.Point(323, 67);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Go";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.bGo.Location = new System.Drawing.Point(323, 67);
+            this.bGo.Name = "bGo";
+            this.bGo.Size = new System.Drawing.Size(75, 23);
+            this.bGo.TabIndex = 0;
+            this.bGo.Text = "Go";
+            this.bGo.UseVisualStyleBackColor = true;
+            this.bGo.Click += new System.EventHandler(this.bGo_Click);
             // 
-            // textBox1
+            // tbEmail
             // 
-            this.textBox1.Location = new System.Drawing.Point(137, 51);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 1;
+            this.tbEmail.Location = new System.Drawing.Point(137, 51);
+            this.tbEmail.Name = "tbEmail";
+            this.tbEmail.Size = new System.Drawing.Size(100, 20);
+            this.tbEmail.TabIndex = 1;
             // 
             // label1
             // 
@@ -62,41 +64,60 @@
             this.label1.TabIndex = 2;
             this.label1.Text = "E-mail";
             // 
-            // textBox2
+            // tbPassword
             // 
-            this.textBox2.Location = new System.Drawing.Point(137, 98);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 3;
+            this.tbPassword.Location = new System.Drawing.Point(137, 87);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.Size = new System.Drawing.Size(100, 20);
+            this.tbPassword.TabIndex = 3;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(50, 103);
+            this.label2.Location = new System.Drawing.Point(52, 87);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(53, 13);
             this.label2.TabIndex = 4;
             this.label2.Text = "Password";
             // 
-            // richTextBox1
+            // rtbOutput
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(55, 179);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(343, 264);
-            this.richTextBox1.TabIndex = 5;
-            this.richTextBox1.Text = "";
+            this.rtbOutput.Location = new System.Drawing.Point(55, 179);
+            this.rtbOutput.Name = "rtbOutput";
+            this.rtbOutput.Size = new System.Drawing.Size(343, 264);
+            this.rtbOutput.TabIndex = 5;
+            this.rtbOutput.Text = "";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(67, 124);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Server";
+            // 
+            // tbServer
+            // 
+            this.tbServer.Location = new System.Drawing.Point(137, 124);
+            this.tbServer.Name = "tbServer";
+            this.tbServer.Size = new System.Drawing.Size(100, 20);
+            this.tbServer.TabIndex = 7;
+            this.tbServer.Text = "imap.gmail.com";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(453, 464);
-            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.tbServer);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.rtbOutput);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.tbPassword);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tbEmail);
+            this.Controls.Add(this.bGo);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -106,12 +127,14 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button bGo;
+        private System.Windows.Forms.TextBox tbEmail;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox tbPassword;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox rtbOutput;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbServer;
     }
 }
 
--- ScrumNotes/Form1.cs
+++ ScrumNotes/Form1.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using S22.Imap;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -17,8 +19,26 @@ namespace ScrumNotes
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void bGo_Click(object sender, EventArgs e)
         {
+
+            // Connect on port 993 using SSL.
+            using (ImapClient client = new ImapClient(tbServer.Text, 993, tbEmail.Text, tbPassword.Text,AuthMethod.Login,true))
+            {
+                Console.WriteLine("We are connected!");
+
+                // Find messages that were sent from abc@def.com and have the string "Hello World" in their subject line.
+                IEnumerable<uint> uids = client.Search(
+                    SearchCondition.Subject("scrum notes")
+                );
+
+                foreach (uint id in uids)
+                {
+                    Console.WriteLine(id);
+                    MailMessage mm= client.GetMessage(id);
+                    rtbOutput.Text = mm.Body;
+                }
+            }
 
         }
     }
